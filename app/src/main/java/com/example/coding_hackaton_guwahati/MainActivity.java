efac49bbe187dc9322a814377ca3e741b454f66e
LavishGulati <lavish00gulati@gmail.com>
2019-12-20 15:02:51 +0530

    Completed user login backend

--- app/src/main/java/com/example/coding_hackaton_guwahati/MainActivity.java
+++ app/src/main/java/com/example/coding_hackaton_guwahati/MainActivity.java
@@ -1,24 +1,36 @@
 package com.example.coding_hackaton_guwahati;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity
 {
     private static final int REQUEST_SIGNUP = 0;
+    private static final int REQUEST_USER_HOME = 1;
 
     TextView signupUserLink;
     EditText emailText, passwdText;
     Button loginBtn;
 
-    int MinPasswdLength = 4;
-    int MaxPasswdLength = 16;
+    int minPassLength = 6;
+    int maxPassLength = 16;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -29,6 +41,7 @@ public class MainActivity extends AppCompatActivity
         signupUserLink = findViewById(R.id.signup_user_link);
         emailText = findViewById(R.id.input_email);
         passwdText = findViewById(R.id.input_password);
+        loginBtn = findViewById(R.id.btn_login);
 
         signupUserLink.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,11 +51,123 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
-        loginButton.setOnClickListener(new View.OnClickListener() {
+        loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 login();
             }
         });
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    private void updateUI(FirebaseUser user) {
+        if(user != null){
+            Intent intent = new Intent(getApplicationContext(), UserHomeActivity.class);
+            startActivityForResult(intent, REQUEST_USER_HOME);
+        }
+    }
+
+    public void login() {
+
+        if(!validate()){
+            return;
+        }
+
+        loginBtn.setEnabled(false);
+
+        final ProgressDialog progressDialog = new ProgressDialog(MainActivity.this);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Authenticating...");
+        progressDialog.show();
+
+        String email = emailText.getText().toString();
+        String password = passwdText.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            //Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            onLoginSuccess();
+                            progressDialog.dismiss();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            //Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            onLoginFailed();
+                            progressDialog.dismiss();
+                            updateUI(null);
+                        }
+
+                    }
+                });
+    }
+
+    public boolean validate() {
+        boolean valid = true;
+
+        String email = emailText.getText().toString();
+        String password = passwdText.getText().toString();
+
+
+        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            emailText.setError("enter a valid email address");
+            valid = false;
+        }
+        else{
+            emailText.setError(null);
+        }
+
+        if (password.isEmpty() || password.length() < minPassLength || password.length() > maxPassLength) {
+            passwdText.setError("between " + minPassLength + " and " + maxPassLength + " alphanumeric characters");
+            valid = false;
+        } else {
+            passwdText.setError(null);
+        }
+
+        if(!valid){
+            Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
+
+        }
+
+        return valid;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_SIGNUP) {
+            if (resultCode == RESULT_OK) {
+                // TODO: Implement successful signup logic here
+                // By default we just finish the Activity and log them in automatically
+            }
+        }
+        else if(requestCode == REQUEST_USER_HOME){
+            if(resultCode == RESULT_OK){
+
+            }
+        }
+    }
+
+    public void onLoginSuccess() {
+        loginBtn.setEnabled(true);
+        Toast.makeText(getBaseContext(), "Login successful", Toast.LENGTH_LONG).show();
+    }
+
+    public void onLoginFailed() {
+        Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
+        loginBtn.setEnabled(true);
     }
 }
---
+++ app/src/main/java/com/example/coding_hackaton_guwahati/UserHomeActivity.java
@@ -0,0 +1,59 @@
+package com.example.coding_hackaton_guwahati;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class UserHomeActivity extends AppCompatActivity {
+
+    Toolbar mTopToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_home);
+
+        mTopToolbar = findViewById(R.id.my_toolbar);
+        mTopToolbar.setTitle("Name of the app");
+        setSupportActionBar(mTopToolbar);
+    }
+
+    @Override
+    public void onBackPressed() {
+        startActivity(new Intent(Intent.ACTION_MAIN).addCategory(Intent.CATEGORY_HOME));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_user_home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_logout:
+                logout();
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+    public void logout(){
+        FirebaseAuth.getInstance().signOut();
+        setResult(RESULT_OK);
+        finish();
+    }
+}
