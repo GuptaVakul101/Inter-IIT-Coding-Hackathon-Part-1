af155b753c5535de75713896b0cc9e850e5f9c9c
cgupta3131 <cgupta3131@gmail.com>
2019-12-20 12:36:04 +0530

    Initialized the project

---
+++ app/src/main/java/com/example/coding_hackaton_guwahati/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.coding_hackaton_guwahati;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
